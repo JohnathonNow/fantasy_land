f809510c15b5e2685a361aca836401d0caa9d27e
John Westhoff <johnjwesthoff@gmail.com>
2018-02-23 11:52:38 -0500

    improved imges`

--- display.py
+++ display.py
@@ -10,7 +10,9 @@ def get():
     return picture
 
 def get_js():
-    return """<img src=\"{}\" style=\"height:100%;width:100%;\" id=\"img\">
+    return """
+    <style>html,body{{margin:0; height:100%;}}img{{display:block; width:100%; height:100%; object-fit: cover;}}</style>
+    <img src=\"{}\" style=\"height:100%;width:100%;\" id=\"img\">
     <script>var i = 0;function a(){{document.getElementById('img').src = window.location.href + 'bg?d='+i; i++; }}a();setInterval(a, 1000);</script>
     """.format(picture)
 
